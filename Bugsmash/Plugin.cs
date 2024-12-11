2e41cbda0f6bf979b9ee071ac756f3adf5d66fd8
irlbunny <ItsKaitlyn03@protonmail.com>
2024-12-11 10:22:36 -0500

    Initialize harmony patches earlier

--- Bugsmash/Plugin.cs
+++ Bugsmash/Plugin.cs
@@ -4,14 +4,9 @@ using System.Reflection;
 
 namespace Bugsmash;
 
-[Plugin(RuntimeOptions.SingleStartInit)]
+[Plugin(RuntimeOptions.SingleStartInit), NoEnableDisable]
 public class Plugin
 {
-    internal static Harmony HarmonyInstance { get; } = new("com.github.whatdahopper.Bugsmash");
-
-    [OnStart]
-    public void OnStart() => HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
-
-    [OnExit]
-    public void OnExit() => HarmonyInstance.UnpatchSelf();
+    [Init]
+    public Plugin() => Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "com.github.whatdahopper.Bugsmash");
 }
