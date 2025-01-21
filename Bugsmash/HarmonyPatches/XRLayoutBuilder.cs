3cf14d6a3b5fea26a732655bf441b7bb6a803e6c
Nicolas Gnyra <nicolas@gnyra.com>
2025-01-20 20:05:28 -0500

    Fix culture-sensitive `ToLower` in `XRLayoutBuilder`

---
+++ Bugsmash/HarmonyPatches/XRLayoutBuilder.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine.InputSystem.XR;
+
+namespace Bugsmash.HarmonyPatches
+{
+    [HarmonyPatch(typeof(XRLayoutBuilder), nameof(XRLayoutBuilder.Build))]
+    internal class XRLayoutBuilderBuild
+    {
+        private static readonly MethodInfo _stringToLowerMethod = AccessTools.DeclaredMethod(typeof(string), nameof(string.ToLower), Array.Empty<Type>());
+        private static readonly MethodInfo _stringToLowerInvariantMethod = AccessTools.DeclaredMethod(typeof(string), nameof(string.ToLowerInvariant));
+
+        protected static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchForward(false, new CodeMatch(i => i.Calls(_stringToLowerMethod)))
+                .ThrowIfInvalid($"string.ToLower() not found")
+                .SetOperandAndAdvance(_stringToLowerInvariantMethod)
+                .InstructionEnumeration();
+        }
+    }
+}
